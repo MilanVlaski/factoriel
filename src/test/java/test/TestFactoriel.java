1407cde305df070ab374c40c26953a5560e637d5
Milan Vlaski <milan.vlaski321@gmail.com>
2024-05-08 10:59:28 +0200

    used static imports

--- src/test/java/test/TestFactoriel.java
+++ src/test/java/test/TestFactoriel.java
@@ -1,23 +1,23 @@
 package test;
 
+import static main.Factorial.factorial;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
-
-import main.Factorial;
 
 public class TestFactoriel
 {
 
 	@Test
 	void of_zero_is_one()
-	{ assertThat(Factorial.factorial(0)).isEqualTo(1); }
+	{ assertThat(factorial(0)).isEqualTo(1); }
 
 	@Test
 	void of_one_is_one()
-	{ assertThat(Factorial.factorial(1)).isEqualTo(1); }
+	{ assertThat(factorial(1)).isEqualTo(1); }
 
 	@Test
 	void of_two_is_two()
-	{ assertThat(Factorial.factorial(2)).isEqualTo(2); }
+	{ assertThat(factorial(2)).isEqualTo(2); }
+
 }
